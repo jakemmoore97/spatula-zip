d7ad06e502f869897a14fb9840e25c6048151fc6
jakemmoore97 <jakemmoore97@gmail.com>
2022-03-23 17:14:52 -0400

    init

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -43,7 +43,7 @@
 
   let alerts = createAlerts({
     zip: {
-      title: 'Wee are not in your city yet, but we’re coming soon!',
+      title: 'Wei are not in your city yet, but we’re coming soon!',
       description:
         'We will send you an email as soon as SPATULA is ready in your city. (ENTER EMAIL)',
     },
