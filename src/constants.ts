efcc5af60647664a04fde65777479fe24619524b
jakemmoore97 <36558108+jakemmoore97@users.noreply.github.com>
2022-03-31 18:00:02 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 
 export const redirectUrl = 'https://www.spatulafoods.com/collections/all'
-export const winUrl = 'https://www.spatulafoods.com/products/build-the-box?'
+export const winUrl = 'https://www.spatulafoods.com/products/build-the-box?selling_plan=749142208'
