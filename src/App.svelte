7108e74bb7a9eafb54859157f46b8938c02c6d18
jakemmoore97 <36558108+jakemmoore97@users.noreply.github.com>
2022-04-21 23:28:31 -0400

    For incognito

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import Image from './assets/homepage.jpg'
   import HeadingGroup from './lib/HeadingGroup.svelte'
-  import Form from './lib/Form.svelte'
+  
   import BackgroundImageCard from './lib/BackgroundImageCard.svelte'
 
   const title = 'Get ready for something good!'
   const description =
-    'We only deliver to certain postal codes. Please enter yours to check current availability.'
+    'Please switch to regular browser to verify your delivery zone'
   const src = Image
   const alt = 'Spatula Foods'
 </script>
 
 <main class="flex font-sans subpixel-antialiased">
   <BackgroundImageCard {src} {alt}>
-    <HeadingGroup {title} {description} />
+    <HeadingGroup  {description} />
     <Form />
   </BackgroundImageCard>
 </main>
